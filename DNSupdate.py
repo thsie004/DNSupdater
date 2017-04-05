6939f9127c82bc310c83a74c560c740add606186
thsie004 <thsie004@flowersync.c.flowersync.internal>
2017-04-05 09:29:03 +0000

    generalized hostname

--- DNSupdate.py
+++ DNSupdate.py
@@ -2,7 +2,7 @@ import sys
 import logging
 import requests
 
-def updateIP(username="", password="", currentIP=""):
+def updateIP(username="", password="", hostname="", currentIP=""):
     #setup logging
     logging.basicConfig(level = logging.INFO)
     logger = logging.getLogger(__name__)
@@ -17,11 +17,11 @@ def updateIP(username="", password="", currentIP=""):
     logger.info('Posting IP update')
 
     url = 'https://domains.google.com/nic/update'
-    data = {'hostname': 'flowersync.com', 'myip': currentIP}
+    data = {'hostname': hostname, 'myip': currentIP}
 
     post_response = requests.post(url, data, auth = (username, password))
 
     logger.info('Posted request with a response of {}'.format(post_response.text))
 
 if __name__ == '__main__':
-    updateIP(username = sys.argv[1], password = sys.argv[2], currentIP = sys.argv[3])
+    updateIP(username = sys.argv[1], password = sys.argv[2], hostname = sys.argv[3], currentIP = sys.argv[4])
